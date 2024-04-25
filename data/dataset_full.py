fix multiclass loss and metric computation

data/dataset_full.py
@@ -236,9 +236,9 @@ class SituationalAwarenessDataset(Dataset):
 
         if self.args.sample_clicks:
             # max time allowed since last click for valid sample
-            self.sample_clicks_min_time = 10 #self.args.secs_of_history                
+            self.sample_clicks_min_time = self.args.secs_of_history                
             # skip samples just before click (to account for reaction time)
-            self.pre_clicks_excl_time = 1
+            self.pre_clicks_excl_time = self.args.pre_clicks_excl_time
 
             # precompute userinput frame nos
             user_input_arr = np.array(self.awareness_df.AwarenessData_UserInput)
@@ -669,7 +669,6 @@ class SituationalAwarenessDataset(Dataset):
             final_ignore_mask = torch.cat((ignore_mask, ignore_mask_left, ignore_mask_right))
         else:
             final_ignore_mask = ignore_mask
-
 
         padded_tensor = torch.nn.functional.pad(final_input_image, (0, 0, 4, 4), mode='constant', value=0)
         padded_label_mask_image_tensor = torch.nn.functional.pad(final_label_mask_image, (0, 0, 4, 4), mode='constant', value=0)

models/custom_train.py
@@ -67,11 +67,21 @@ class Epoch:
 
                 # for metric computation, use masked prediction and GT
                 if self.loss.mode == 'multiclass':
-                    # print(y.shape)
+                    # need to activate the prediction for metric computation
+                    y_pred = y_pred.log_softmax(dim=1).exp()
+                    # make ground truth one-hot
+                    # N, 1, H, W -> N, C, H, W
                     y = F.one_hot(y.view(y.shape[0], y.shape[2], y.shape[3]), num_classes=y_pred.size(1)).permute(0, 3, 1, 2).float()
-                    y_pred[:, 0, :, :] *= mask[:, 0, :, :] # only multiply with mask on the aware channel
-                    y[:, 0, :, :] *= mask[:, 0, :, :]
+                    # multiply with mask to ignore old clicks
+                    y_pred *= mask 
+                    y *= mask
+                    # exclude background class for metric calculation+
+                    # sometimes this means for a pixel, GT/pred can be 0,0 which is fine
+                    y_pred = y_pred[:, :2,...]
+                    y = y[:, :2,...]
                 else:
+                    # need to activate the prediction for metric computation
+                    y_pred = F.logsigmoid(y_pred).exp()
                     y_pred = y_pred*mask
                     y = y*mask
 
@@ -107,12 +117,8 @@ class TrainEpoch(Epoch):
     def batch_update(self, x, y, mask):
         self.optimizer.zero_grad()
         prediction = self.model.forward(x)
-        if self.loss.mode == 'multiclass':
-            prediction[:, 0, :, :] *= mask[:, 0, :, :] # only multiply with mask on the aware channel
-            y[:, 0, :, :] *= mask[:, 0, :, :]
-            loss = self.loss(prediction, y)
-        else:
-            loss = self.loss(prediction*mask, y*mask)
+        # multiply with mask to ignore old clicks
+        loss = self.loss(prediction*mask, y*mask)
         loss.backward()
         self.optimizer.step()
         return loss, prediction
@@ -135,10 +141,6 @@ class ValidEpoch(Epoch):
     def batch_update(self, x, y, mask):
         with torch.no_grad():
             prediction = self.model.forward(x)
-            if self.loss.mode == 'multiclass':
-                prediction[:, 0, :, :] *= mask[:, 0, :, :] # only multiply with mask on the aware channel
-                y[:, 0, :, :] *= mask[:, 0, :, :]
-                loss = self.loss(prediction, y)
-            else:
-                loss = self.loss(prediction*mask, y*mask)
+            # multiply with mask to ignore old clicks
+            loss = self.loss(prediction*mask, y*mask)
         return loss, prediction

models/smp_integrated_test.py
@@ -121,6 +121,9 @@ def main(args):
                     from_logits=True, smooth=0.0,
                     ignore_index=None, eps=1e-7, class_weights=class_weights)
 
+
+    # note: metric computations expect activated predictions
+    # we do activations in the loss function, so custom_train implements it prior to metric computation
     metrics = [
         smp_utils.metrics.IoU(threshold=0.5),
     ]
@@ -164,8 +167,8 @@ def main(args):
                 wandb.log({"valid_"+k: valid_logs[k]})
 
         # do something (save model, change lr, etc.)
-        if max_score < train_logs['iou_score']:
-            max_score = train_logs['iou_score']
+        if max_score < valid_logs['iou_score']:
+            max_score = valid_logs['iou_score']
             if args.wandb:
                 torch.save(model,
                     os.path.join(wandb.run.dir, './best_model_%s.pth' 
@@ -214,8 +217,8 @@ if __name__ == "__main__":
     args.add_argument("--sample-clicks", choices=['post_click', 'pre_excl', 'both', ''], 
                       default='', help="Empty string -> sample everything")
     args.add_argument("--ignore-oldclicks", action='store_true')
-    # empty string is sample everything
-    args.add_argument("--weighted-unaware-sampling", action='store_true')
+    args.add_argument("--weighted-unaware-sampling", action='store_true', help="equally sample images with atleast one unaware obj and images with no unaware obj")
+    args.add_argument("--pre-clicks-excl-time", type=float, default=1.0, help="seconds before click to exclude for reaction time")
     args.add_argument("--unaware-classwt", type=float, default=1.0)
     args.add_argument("--bg-classwt", type=float, default=1e-5)
 